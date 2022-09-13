2714bdf54c94657f49b6e1a9dc6a15e7e9dadbf9
Vishwanath Veerni <vishwanath.veerni@gmail.com>
2022-09-13 09:56:03 +0530

    encrypt PRIVATE_KEY

---
+++ ethers-simple-storage/encryptKey.js
@@ -0,0 +1,18 @@
+require("dotenv").config();
+const ethers = require("ethers");
+const fs = require("fs-extra");
+
+async function main() {
+  const wallet = new ethers.Wallet(process.env.PRIVATE_KEY);
+  const encryptedJsonKey = await wallet.encrypt(
+    process.env.PRIVATE_KEY_PASSWORD,
+    process.env.PRIVATE_KEY
+  );
+
+  console.log(encryptedJsonKey);
+  fs.writeFileSync("./.encryptedKey.json", encryptedJsonKey);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((err) => process.exit(1));
