b9fc0e3c2c53fff523da4ae38f8a88708d857789
Vishwanath Veerni <vishwanath.veerni@gmail.com>
2022-09-17 10:45:05 +0530

    Add goerli network in hardhat config

--- hardhat-simple-storage/hardhat.config.js
+++ hardhat-simple-storage/hardhat.config.js
@@ -1,6 +1,16 @@
-require("@nomicfoundation/hardhat-toolbox");
+require("@nomicfoundation/hardhat-toolbox")
+require("dotenv").config()
 
 /** @type import('hardhat/config').HardhatUserConfig */
+
+const GOERLI_RCP_URL = process.env.GOERLI_RCP_URL
+const GOERLI_PRIVATE_KEY = process.env.GOERLI_PRIVATE_KEY
+
 module.exports = {
-  solidity: "0.8.8",
-};
+    defaultNetwork: "hardhat",
+    goerli: {
+        url: GOERLI_RCP_URL,
+        accounts: [GOERLI_PRIVATE_KEY],
+    },
+    solidity: "0.8.8",
+}
