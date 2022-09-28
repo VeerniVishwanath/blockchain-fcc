e25c30a2aafe20fb212aec2517e5b1d130c3c03b
Vishwanath Veerni <vishwanath.veerni@gmail.com>
2022-09-28 17:11:23 +0530

    <---------- Initial Setup- Lottery ---------->

---
+++ 5-hardhat-smartcontract-lottery/contracts/Raffle.sol
@@ -0,0 +1,15 @@
+/** Raffle
+ * Enter the Lottery (By paying some eth)
+ * Pick a random winner (verifiably random)
+ * Winner will be selected every X minutes -> completly automate
+ * ChainLink Oracle -> Randomness, Automated Execution (Chainlink Keepers)
+*/
+
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.7;
+
+contract Raffle {
+    function enterRaffle(){}
+
+    // function pickRandomWinner(){}
+}
---
+++ 5-hardhat-smartcontract-lottery/hardhat.config.js
@@ -0,0 +1,15 @@
+require("@nomiclabs/hardhat-waffle")
+require("@nomiclabs/hardhat-etherscan")
+require("hardhat-deploy")
+require("solidity-coverage")
+require("hardhat-gas-reporter")
+require("hardhat-contract-sizer")
+require("dotenv").config()
+
+/** @type import('hardhat/config').HardhatUserConfig */
+
+module.exports = {
+    solidity: {
+        compilers: [{ version: "0.8.7" }],
+    },
+}
