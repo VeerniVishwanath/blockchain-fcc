9337573e79cd5f4abef5cd5757a6a8008530d1ce
Vishwanath Veerni <vishwanath.veerni@gmail.com>
2022-09-28 08:58:26 +0530

    Add functions for connect and fund

---
+++ 4-html-fund-me/index.js
@@ -0,0 +1,58 @@
+import { ethers } from "./ethers-5.2.esm.min.js"
+import { contractAddress, abi } from "./constants.js"
+
+const connectButton = document.getElementById("connectButton")
+const fundButton = document.getElementById("fundButton")
+
+connectButton.onclick = connect
+fundButton.onclick = fund
+
+async function connect() {
+    if (typeof window.ethereum !== "undefined") {
+        try {
+            await window.ethereum.request({ method: "eth_requestAccounts" })
+            connectButton.innerHTML = "Connected!!"
+        } catch (err) {
+            console.log(err)
+        }
+    } else {
+        connectButton.innerHTML = "Please install MetaMask!!"
+    }
+}
+
+async function fund() {
+    const ethAmount = "77"
+    console.log(`Funding with ${ethAmount}..`)
+    if (typeof window.ethereum !== "undefined") {
+        //provider / connection to blockchain
+        const provider = new ethers.providers.Web3Provider(window.ethereum)
+        //signer / wallet / someone with gas
+        const signer = provider.getSigner()
+        //contract
+        //^ ABI, Address
+        const contract = new ethers.Contract(contractAddress, abi, signer)
+        try {
+            const txResponse = await contract.fund({
+                value: ethers.utils.parseEther(ethAmount),
+            })
+            await listenForTransactionMine(txResponse, provider)
+            console.log("Done!!")
+        } catch (error) {
+            console.log(`Error: ${error}`)
+        }
+    }
+}
+
+function listenForTransactionMine(txResponse, provider) {
+    console.log(`Mining: ${txResponse.hash}....`)
+    //listen for the transaction to finish
+    return new Promise((resolve, reject) => {
+        provider.once(txResponse.hash, (txReceipt) => {
+            console.log(
+                `Completed with ${txReceipt.confirmations} Confirmations`
+            )
+            resolve()
+        })
+    })
+}
+
