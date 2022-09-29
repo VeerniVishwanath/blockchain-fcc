ebc3ccc7a8666bc3e687223df19731aa3c406352
Vishwanath Veerni <vishwanath.veerni@gmail.com>
2022-09-29 08:44:07 +0530

    Add code to enter the Raffle

--- 5-hardhat-smartcontract-lottery/contracts/Raffle.sol
+++ 5-hardhat-smartcontract-lottery/contracts/Raffle.sol
@@ -3,13 +3,40 @@
  * Pick a random winner (verifiably random)
  * Winner will be selected every X minutes -> completly automate
  * ChainLink Oracle -> Randomness, Automated Execution (Chainlink Keepers)
-*/
+ */
 
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.7;
+pragma solidity ^0.8.7;
+
+/**Error codes */
+error Raffle__NotEnoughETHEntered();
 
 contract Raffle {
-    function enterRaffle(){}
+    /**State Variables */
+    uint256 private immutable i_entranceFee;
+    address payable[] private s_players;
+
+    /**Constructor */
+    constructor(uint256 entranceFee) {
+        i_entranceFee = entranceFee;
+    }
+
+    /**Entering the Lottery/Raffle */
+    function enterRaffle() public payable {
+        if (msg.value < i_entranceFee) {
+            revert Raffle__NotEnoughETHEntered();
+        }
+        s_players.push(payable(msg.sender));
+    }
 
     // function pickRandomWinner(){}
+
+    /**View and Pure functions */
+    function getEntranceFee() public view returns (uint256) {
+        return i_entranceFee;
+    }
+
+    function getPlayer(uint256 index) public view returns (address) {
+        return s_players[index];
+    }
 }
