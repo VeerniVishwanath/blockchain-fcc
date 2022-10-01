cf3a8d34cda9513fb2bdf29a149a7da8aa42cd1c
Vishwanath Veerni <vishwanath.veerni@gmail.com>
2022-10-01 18:08:40 +0530

    Implementing Chainlink VRF - The Request

--- 5-hardhat-smartcontract-lottery/contracts/Raffle.sol
+++ 5-hardhat-smartcontract-lottery/contracts/Raffle.sol
@@ -8,23 +8,44 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
 
-/**Error codes */
+/* Imports */
+import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+
+/* Error codes */
 error Raffle__NotEnoughETHEntered();
 
-contract Raffle {
-    /**State Variables */
+contract Raffle is VRFConsumerBaseV2 {
+    /* State Variables */
     uint256 private immutable i_entranceFee;
     address payable[] private s_players;
+    VRFCoordinatorV2Interface private immutable i_vrfCoordinator;
+    bytes32 private immutable i_gasLane;
+    uint64 private immutable i_subscriptionId;
+    uint32 private immutable i_callbackGasLimit;
+    uint16 private constant REQUEST_CONFIRMATIONS = 3;
+    uint32 private constant NUMWORDS = 1;
 
-    /**Events */
+    /* Events */
     event RaffleEnter(address indexed player);
+    event RequestedRaffleWinner(uint256 indexed requestId);
 
-    /**Constructor */
-    constructor(uint256 entranceFee) {
+    /* Constructor */
+    constructor(
+        address vrfCoordinatorV2,
+        uint256 entranceFee,
+        bytes32 gasLane,
+        uint64 subscriptionId,
+        uint32 callbackGasLimit
+    ) VRFConsumerBaseV2(vrfCoordinatorV2) {
         i_entranceFee = entranceFee;
+        i_vrfCoordinator = VRFCoordinatorV2Interface(vrfCoordinatorV2);
+        i_gasLane = gasLane;
+        i_subscriptionId = subscriptionId;
+        i_callbackGasLimit = callbackGasLimit;
     }
 
-    /**Entering the Lottery/Raffle */
+    /* Entering the Lottery/Raffle */
     function enterRaffle() public payable {
         if (msg.value < i_entranceFee) {
             revert Raffle__NotEnoughETHEntered();
@@ -35,9 +56,26 @@ contract Raffle {
         emit RaffleEnter(msg.sender);
     }
 
-    // function pickRandomWinner(){}
+    function requestRandomWinner() external {
+        //request the random number
+        //Once we get it do something with it
+        //2 transaction process
+        uint256 requestId = i_vrfCoordinator.requestRandomWords(
+            i_gasLane, // keyHash
+            i_subscriptionId,
+            REQUEST_CONFIRMATIONS,
+            i_callbackGasLimit,
+            NUMWORDS
+        );
+        emit RequestedRaffleWinner(requestId);
+    }
 
-    /**View and Pure functions */
+    function fulfillRandomWords(uint256 requestId, uint256[] memory randomWords)
+        internal
+        override
+    {}
+
+    /* View and Pure functions */
     function getEntranceFee() public view returns (uint256) {
         return i_entranceFee;
     }
