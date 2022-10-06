ae488d710095777d62d99f107d15adc0d1af2cbf
Vishwanath Veerni <vishwanath.veerni@gmail.com>
2022-10-06 21:48:15 +0530

    Add hardhat config

--- 5-hardhat-smartcontract-lottery/hardhat.config.js
+++ 5-hardhat-smartcontract-lottery/hardhat.config.js
@@ -6,10 +6,34 @@ require("hardhat-gas-reporter")
 require("hardhat-contract-sizer")
 require("dotenv").config()
 
-/** @type import('hardhat/config').HardhatUserConfig */
+const GORELI_RPC_URL = process.env.GORELI_RPC_URL
+const PRIVATE_KEY = process.env.PRIVATE_KEY
+// const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
 
 module.exports = {
+    defaultNetworks: "hardhat",
+    networks: {
+        hardhat: {
+            chainId: 31337,
+            blockConfirmations: 1,
+        },
+        goerli: {
+            chainId: 420,
+            blockConfirmations: 6,
+            url: GORELI_RPC_URL,
+            accounts: [PRIVATE_KEY],
+        },
+    },
     solidity: {
         compilers: [{ version: "0.8.7" }],
+    },
+    namedAccounts: {
+        deployer: {
+            default: 0,
+        },
+        player: {
+            default: 1,
+        },
     },
 }
