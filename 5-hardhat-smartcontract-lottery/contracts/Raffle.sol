75a1d38b0c391425902b98ae10c374b397ecbe19
Vishwanath Veerni <vishwanath.veerni@gmail.com>
2022-10-10 21:31:28 +0530

    Add getInterval view Function

--- 5-hardhat-smartcontract-lottery/contracts/Raffle.sol
+++ 5-hardhat-smartcontract-lottery/contracts/Raffle.sol
@@ -181,7 +181,11 @@ contract Raffle is VRFConsumerBaseV2, KeeperCompatibleInterface {
         return s_lastTimePassed;
     }
 
-    function getRequestConfirmations() public pure returns (uint256){
+    function getRequestConfirmations() public pure returns (uint256) {
         return REQUEST_CONFIRMATIONS;
+    }
+
+    function getInterval() public view returns (uint256) {
+        return i_interval;
     }
 }
