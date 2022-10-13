8b48641ddf57d3aeb5ac06c715ae9d415a3fb25b
Vishwanath Veerni <vishwanath.veerni@gmail.com>
2022-10-13 23:43:15 +0530

    Change networkId for goerli

--- 5-hardhat-smartcontract-lottery/helper-hardhat-config.js
+++ 5-hardhat-smartcontract-lottery/helper-hardhat-config.js
@@ -1,12 +1,12 @@
 const { ethers } = require("hardhat")
 
 const networkConfig = {
-    420: {
+    5: {
         name: "goerli",
         vrfCoordinatorV2: "0x2ca8e0c643bde4c2e08ab1fa0da3401adad7734d",
         entranceFee: ethers.utils.parseEther("0.1"),
         gasLane: "0x79d3d8832d904592c0bf9818b621522c988bb8b0c05cdc3b15aea1b6e8db0c15",
-        subscriptionIs: "3064",
+        subscriptionId: "3064",
         callbackGaslimit: "2500000", //2,500,000
         interval: "30",
     },
