6b156d7057d4e5859d44ab6bbf2780d3e81b7784
Vishwanath Veerni <vishwanath.veerni@gmail.com>
2022-10-21 17:13:07 +0530

    Add web3uikit

--- 6-nextjs-smartcontract-lottery/pages/_app.js
+++ 6-nextjs-smartcontract-lottery/pages/_app.js
@@ -1,10 +1,13 @@
 import { MoralisProvider } from "react-moralis"
 import "../styles/globals.css"
+import { NotificationProvider } from "web3uikit"
 
 function MyApp({ Component, pageProps }) {
     return (
         <MoralisProvider initializeOnMount={false}>
-            <Component {...pageProps} />
+            <NotificationProvider>
+                <Component {...pageProps} />
+            </NotificationProvider>
         </MoralisProvider>
     )
 }
