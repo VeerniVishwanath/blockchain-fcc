f76182d906c7b3ec6a2f01527a5220f5abf33b6c
Vishwanath Veerni <vishwanath.veerni@gmail.com>
2022-10-23 21:24:45 +0530

    Add tailwindcss

--- 6-nextjs-smartcontract-lottery/components/Header.jsx
+++ 6-nextjs-smartcontract-lottery/components/Header.jsx
@@ -2,9 +2,13 @@ import { ConnectButton } from "web3uikit"
 
 export default function Header() {
     return (
-        <div>
-            Decentralized Lottery 
-            <ConnectButton moralisAuth={false} />
+        <div className="border-b-2 p-5 flex flex-row">
+            <h1 className="py-4 px-4 font-blog text-3xl ">
+                Decentralized Lottery
+            </h1>
+            <div className="ml-auto py-2 px-4">
+                <ConnectButton moralisAuth={false} />
+            </div>
         </div>
     )
 }
