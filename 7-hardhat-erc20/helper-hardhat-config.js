be19bcefa5fc855252541d27975b9e40007fa838
Vishwanath Veerni <vishwanath.veerni@gmail.com>
2022-10-26 23:10:39 +0530

    Add helper hardhat config

---
+++ 7-hardhat-erc20/helper-hardhat-config.js
@@ -0,0 +1,13 @@
+const networkConfig = {
+    5: {
+        name: "goerli",
+    },
+    31337: {
+        name: localhost,
+    },
+}
+
+const developmentChains = ["localhost", "hardhat"]
+const INITIAL_SUPPLY = "1000000000000000000000000"
+
+module.exports = { networkConfig, developmentChains, INITIAL_SUPPLY }
