424001c1c70106d48f3ae76cab66577531eea346
Vishwanath Veerni <vishwanath.veerni@gmail.com>
2022-10-26 23:10:13 +0530

    Add  hardhat configs

--- 7-hardhat-erc20/hardhat.config.js
+++ 7-hardhat-erc20/hardhat.config.js
@@ -1,6 +1,34 @@
-/** @type import('hardhat/config').HardhatUserConfig */
+require("hardhat-deploy")
+require("dotenv").config()
+
+const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL
 module.exports = {
-  solidity: {
-    compilers: [{ version: "0.8.7" }],
-  },
-};
+    defaultNetwork: "hardhat",
+    networks: {
+        localhost: {
+            chainId: 31337,
+            blockConfirmations: 1,
+        },
+        hardhat: {
+            chainId: 31337,
+            blockConfirmations: 1,
+        },
+        goerli: {
+            chainId: 5,
+            url: GOERLI_RPC_URL,
+            blockConfirmations: 6,
+        },
+    },
+    solidity: {
+        compilers: [{ version: "0.8.7" }],
+    },
+    namedAccounts: {
+        deployer: {
+            default: 0, // here this will by default take the first account as deployer
+            1: 0, // similarly on mainnet it will take the first account as deployer. Note though that depending on how hardhat network are configured, the account 0 on one network can be different than on another
+        },
+        user1: {
+            default: 1,
+        },
+    },
+}
