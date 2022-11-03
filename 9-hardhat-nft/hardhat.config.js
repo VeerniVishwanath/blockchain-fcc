8771e636e75188fdacfef18e2cead6f47e9e82f7
Vishwanath Veerni <vishwanath.veerni@gmail.com>
2022-11-03 22:07:53 +0530

    <---------- Initial Setup- Hardhat Nft ---------->

---
+++ 9-hardhat-nft/hardhat.config.js
@@ -0,0 +1,63 @@
+require("@nomiclabs/hardhat-waffle")
+require("@nomiclabs/hardhat-etherscan")
+require("hardhat-deploy")
+require("solidity-coverage")
+require("hardhat-gas-reporter")
+require("hardhat-contract-sizer")
+require("dotenv").config()
+
+const GORELI_RPC_URL = process.env.GORELI_RPC_URL
+const PRIVATE_KEY = process.env.PRIVATE_KEY
+// const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
+const MAINNET_RPC_URL = process.env.MAINNET_RPC_URL
+
+module.exports = {
+    defaultNetworks: "hardhat",
+    networks: {
+        localhost: {
+            chainId: 31337,
+            blockConfirmations: 1,
+        },
+        hardhat: {
+            chainId: 31337,
+            blockConfirmations: 1,
+            forking: {
+                url: MAINNET_RPC_URL,
+            },
+        },
+        goerli: {
+            chainId: 5,
+            blockConfirmations: 6,
+            url: GORELI_RPC_URL,
+            accounts: [PRIVATE_KEY],
+        },
+    },
+    solidity: {
+        compilers: [{ version: "0.8.7" }, { version: "0.4.19" }, { version: "0.6.12" }],
+    },
+    namedAccounts: {
+        deployer: {
+            default: 0,
+        },
+        user1: {
+            default: 1,
+        },
+    },
+    gasReporter: {
+        enabled: false,
+        currency: "USD",
+        outputFile: "gas-report.txt",
+        noColors: true,
+        // coinmarketcap: process.env.COINMARKETCAP_API_KEY,
+    },
+    mocha: {
+        timeout: 500000, // 500 seconds max for running tests
+    },
+    etherscan: {
+        // yarn hardhat verify --network <NETWORK> <CONTRACT_ADDRESS> <CONSTRUCTOR_PARAMETERS>
+        apiKey: {
+            goerli: ETHERSCAN_API_KEY,
+        },
+    },
+}
