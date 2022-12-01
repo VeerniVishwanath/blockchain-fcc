cbf6ae785aed9c50fdfa1750f64b8b0e7a80d63d
Vishwanath Veerni <vishwanath.veerni@gmail.com>
2022-12-01 21:19:00 +0530

    Add Etherscan verify script

---
+++ 10-hardhat-nft-marketplace/utils/verify.js
@@ -0,0 +1,19 @@
+const verify = async ({ run }, contractAddress, args) => {
+    console.log("Verifying contract...")
+    try {
+        await run("verify:verify", {
+            address: contractAddress,
+            constructorArguments: args,
+        })
+    } catch (e) {
+        if (e.message.toLowerCase().includes("already verified")) {
+            console.log("Already verified!")
+        } else {
+            console.log(e)
+        }
+    }
+}
+
+module.exports = {
+    verify,
+}
