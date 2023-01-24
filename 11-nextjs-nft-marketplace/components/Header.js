a7ac6cc52a1953ab928435b0c7cc880127aaa2e1
Vishwanath Veerni <vishwanath.veerni@gmail.com>
2023-01-24 08:35:16 +0530

    updated

--- 11-nextjs-nft-marketplace/components/Header.js
+++ 11-nextjs-nft-marketplace/components/Header.js
@@ -1,16 +1,16 @@
-import Link from "next/link"
 import { ConnectButton } from "web3uikit"
+import Link from "next/link"
 
 export default function Header() {
     return (
         <nav className="p-5 border-b-2 flex flex-row justify-between items-center">
-            <h1 className="p-4 font-bold text-3xl">NFT Marketplace</h1>
-            <div className="items-center flex flex-row">
-                <Link href="/" className="mr-4 p-6">
-                    Home
+            <h1 className="py-4 px-4 font-bold text-3xl">NFT Marketplace</h1>
+            <div className="flex flex-row items-center">
+                <Link href="/">
+                    <a className="mr-4 p-6">Home</a>
                 </Link>
-                <Link href="/sell-nft" className="mr-4 p-6">
-                    Sell Nft
+                <Link href="/sell-nft">
+                    <a className="mr-4 p-6">Sell NFT</a>
                 </Link>
                 <ConnectButton moralisAuth={false} />
             </div>
